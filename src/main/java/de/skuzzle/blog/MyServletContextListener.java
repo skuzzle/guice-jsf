38b8e98ea02333500654eef3299a2ed87aef5ba2
Simon Taddiken <simon.taddiken@gmail.com>
2015-06-20 09:11:51 +0200

    remove unnecessary code

--- src/main/java/de/skuzzle/blog/MyServletContextListener.java
+++ src/main/java/de/skuzzle/blog/MyServletContextListener.java
@@ -3,7 +3,6 @@ package de.skuzzle.blog;
 import javax.servlet.ServletContextEvent;
 
 import org.apache.myfaces.el.unified.resolver.GuiceResolver;
-import org.apache.myfaces.webapp.StartupServletContextListener;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -17,7 +16,6 @@ import com.google.inject.servlet.GuiceServletContextListener;
 public class MyServletContextListener extends GuiceServletContextListener {
 
     private final Injector injector;
-    private StartupServletContextListener startupListener;
 
     public MyServletContextListener() {
         // List all your modules here
@@ -34,12 +32,6 @@ public class MyServletContextListener extends GuiceServletContextListener {
         // this sets up guice servlet
         super.contextInitialized(event);
 
-        // Get hold of the StartupServletContextListener and call it after Guice
-        // has been initialized. This will perform all necessary JSF setup
-        this.startupListener = this.injector.getInstance(
-                StartupServletContextListener.class);
-        this.startupListener.contextInitialized(event);
-
         // place injector into the context for the GuiceResolver to find
         event.getServletContext().setAttribute(GuiceResolver.KEY, this.injector);
     }
@@ -48,7 +40,6 @@ public class MyServletContextListener extends GuiceServletContextListener {
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
         // properly tear down
         super.contextDestroyed(servletContextEvent);
-        this.startupListener.contextDestroyed(servletContextEvent);
     }
 
 }
